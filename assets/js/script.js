5e7dcf10c5337111b96b8b36fb9004174da14f24
Bennett Collier <bennettcollier@Bennetts-MacBook-Pro.local>
2020-11-12 18:50:03 -0800

    column class

--- assets/js/script.js
+++ assets/js/script.js
@@ -88,13 +88,13 @@ var renderCities = function (cList) {
 var populateNow = function (cityObj, UVindex) {
 
     UVi = parseInt(UVindex)
-    if (UVi <= 2) {
+    if (UVi <= 2.00) {
         uvclass = "uv-low"
-    } else if (UVi <= 5) {
+    } else if (UVi <= 5.00) {
         uvclass = "uv-moderate"
-    } else if (UVi <= 7) {
+    } else if (UVi <= 7.00) {
         uvclass = "uv-high"
-    } else if (UVi <= 10) {
+    } else if (UVi <= 10.00) {
         uvclass = "uv-very-high"
     } else {
         uvclass = "uv-extreme"
@@ -106,19 +106,20 @@ var populateNow = function (cityObj, UVindex) {
 
 
     // temperature conversion
-    var tC = cityObj.main.temp - 273.15
+    // var tC = cityObj.main.temp - 273.15
     var tK = (cityObj.main.temp - 273.15) * 9 / 5 + 32
-    var tempStr = tC.toFixed(1) + "&#176C / " + tK.toFixed(1) + "&#176F";
+    var tempStr = tK.toFixed(1) + "&#176F";
+    // var tempStr = tC.toFixed(1) + "&#176C / " + tK.toFixed(1) + "&#176F";
 
     var cardEl = document.createElement("div");
     cardEl.setAttribute("class", "card");
 
     var htmlStr = `<div class = "card-body">
-        <h2 id="city-now" class = "card-title">${cityObj.name} : ${moment.unix(cityObj.dt).format("MMM Do YYYY")}</h5>
+        <h2 id="city-now" class = "card-title">${cityObj.name}  (${moment.unix(cityObj.dt).format("MM/DD/YYYY")})</h5>
         <img src="${iconurl}" alt="">
         <p class="card-text">Temperature: ${tempStr}</p>
-        <p class="card-text">Humidity: ${cityObj.main.humidity}% relative </p>
-        <p class="card-text">Wind Speed: ${cityObj.wind.speed}m/s / ${(cityObj.wind.speed * 1.94384).toFixed(1)}kts  <span id ="arrow" style="--angle: ${cityObj.wind.deg}deg">&#8679;</span>  </p>
+        <p class="card-text">Humidity: ${cityObj.main.humidity}% </p>
+        <p class="card-text">Wind Speed: ${cityObj.wind.speed} MPS</p>
         <p class="card-text">UV Index:  <span class = "badge ${uvclass}">${UVindex}</span> </p>
     </div>`;
 
@@ -150,7 +151,7 @@ var populateForecast = function (fcastObj) {
         cardEl = populateForecastCard(chunk);
         document.getElementById("forecastcards").appendChild(cardEl);
     };
-    document.getElementById("five-days").innerHTML = "<h2>5 day forecast</h2>"
+    document.getElementById("five-days").innerHTML = "<h2>5-Day Forecast:</h2>"
 };
 
 
@@ -172,7 +173,7 @@ var populateForecastCard = function (chunkObj) {
     };
 
     timeStr = moment.unix(twoPmEl.dt).format("h:a")
-    dateStr = moment.unix(twoPmEl.dt).format("MMM Do");
+    dateStr = moment.unix(twoPmEl.dt).format("MM/DD/YYYY");
 
 
     var rowEl = document.createElement("div");
@@ -181,18 +182,7 @@ var populateForecastCard = function (chunkObj) {
         var hrCol = document.createElement("div");
         hrCol.setAttribute("class", "hr-meteogram-el")
 
-        hrCol.innerHTML =
-            `
-        <div>
-            ${moment.unix(time.dt).format("h")}</br>
-            ${moment.unix(time.dt).format("a")}</br>
-            ${((time.main.temp - 273.15) * 9 / 5 + 32).toFixed(0) + "&#176"} </br> 
-            <img src="${"https://openweathermap.org/img/w/" + time.weather[0].icon + ".png"}" alt=""></br>  
-            <span id ="arrow" style="--angle: ${time.wind.deg}deg">&#8679;</span> </br>
-            ${Math.round((time.wind.speed) * 1.94384)}.kt
-            
-        </div>
-        `
+
 
         rowEl.appendChild(hrCol)
 
@@ -202,14 +192,15 @@ var populateForecastCard = function (chunkObj) {
     <div class="card bg-primary text-light" style="width: 12rem;">
         <div class="card-body">
             <div class = "row" >
-                <h5 class="col card-title">${dateStr} ${timeStr}</h5>
+                <h5 class="col card-title">${dateStr}</h5>
             </div>
             <div class = "row">
                 <div class="col">
                    
-                    ${((twoPmEl.main.temp - 273.15) * 9 / 5 + 32).toFixed(1) + "&#176F"}  
-                    <img src="${"https://openweathermap.org/img/w/" + twoPmEl.weather[0].icon + ".png"}" alt="">   
-                    Humidity: ${twoPmEl.main.humidity}% rel.                    
+                      
+                    <img src="${"https://openweathermap.org/img/w/" + twoPmEl.weather[0].icon + ".png"}" alt=""> <br>
+                    Temp: ${((twoPmEl.main.temp - 273.15) * 9 / 5 + 32).toFixed(1) + "&#176F"}
+                    Humidity: ${twoPmEl.main.humidity}%                    
                 </div>
             
             </div>
